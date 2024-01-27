6e04b3b4985ee918df12fd2eefdb427433699a87
joeTid09 <joseph.tidwell09@gmail.com>
2024-01-27 16:56:04 -0600

    scaffolded controllers for CRUD to db

---
+++ Controllers/PicturesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Plants_Api.Contexts;
+using Plants_Api.Models;
+
+namespace Plants_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly PlantsContext _context;
+
+        public PicturesController(PlantsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pictures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Picture>>> GetPictures()
+        {
+            return await _context.Pictures.ToListAsync();
+        }
+
+        // GET: api/Pictures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Picture>> GetPicture(int id)
+        {
+            var picture = await _context.Pictures.FindAsync(id);
+
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return picture;
+        }
+
+        // PUT: api/Pictures/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPicture(int id, Picture picture)
+        {
+            if (id != picture.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(picture).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PictureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pictures
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Picture>> PostPicture(Picture picture)
+        {
+            _context.Pictures.Add(picture);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPicture", new { id = picture.Id }, picture);
+        }
+
+        // DELETE: api/Pictures/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePicture(int id)
+        {
+            var picture = await _context.Pictures.FindAsync(id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pictures.Remove(picture);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PictureExists(int id)
+        {
+            return _context.Pictures.Any(e => e.Id == id);
+        }
+    }
+}
